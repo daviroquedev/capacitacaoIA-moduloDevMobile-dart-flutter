9644657e0e9cfe2d2789c6d362b2a7c9cae091e0
Davi Roque <101668192+daviroquedev@users.noreply.github.com>
2023-03-31 09:43:27 -0300

    Update removeWhere.dart

--- codes-study/removeWhere.dart
+++ codes-study/removeWhere.dart
@@ -1,6 +1,7 @@
 void main(){
   final numbers = <String>['one', 'two', 'three', 'four', 'five', 'six','two','oito','dois','two'];
   print(numbers);
+  //removeWhere remove todos os itens de acordo com o retorno da arrow function.
   numbers.removeWhere((item) => item == "two");
   print(numbers);
 }
