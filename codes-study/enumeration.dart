9a9dbe6607e47669f60cd1f8500c1366ff98685a
Davi Roque <101668192+daviroquedev@users.noreply.github.com>
2023-03-31 11:12:37 -0300

    enum study

---
+++ codes-study/enumeration.dart
@@ -0,0 +1,17 @@
+//um conjunto de valores constante
+
+enum Status {
+  Carregando,
+  Carregado,
+  Finalizado
+}
+
+void main() {
+  print(Status.Carregado.name); // Carregado
+  print(Status.Finalizado.index); // 2
+
+
+  //criando uma lista com os status
+  //List<Status>
+
+}
